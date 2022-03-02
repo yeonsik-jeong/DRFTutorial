32ab06b65277c3807cf896f58d51be8d03815a17
Yeonsik Jeong <yeonsik.jeong@gmail.com>
2022-03-02 18:57:52 +0900

    Update the DRFTutorial project: 2. Requests and Responses, 03/02/2022

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.listSnippets),
     path('snippets/<int:pk>/', views.detailSnippet),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
