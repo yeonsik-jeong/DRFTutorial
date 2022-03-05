2c23e1f0c62034e895f731935d4535b5a4e0da1b
Yeonsik Jeong <yeonsik.jeong@gmail.com>
2022-03-06 00:18:04 +0900

    Update the DRFTutorial project: 6. ViewSets & Routers, 03/06/2022

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,56 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
+""" 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+] 
+"""
+
+ 
+snippet_list = views.SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail = views.SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight = views.SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = views.UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail = views.UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = [
+    path('snippets/', snippet_list, name='snippetList'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippetDetail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippetHighlight'),
+    path('users/', user_list, name='userList'),
+    path('users/<int:pk>/', user_detail, name='userDetail'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', views.apiIndex),
+] 
+
+
+"""
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippetList'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippetDetail'),
@@ -11,5 +60,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.apiIndex),
 ]
+"""
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -3,12 +3,13 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.views import APIView
 from rest_framework import mixins, generics
 from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
@@ -21,7 +22,24 @@ def apiIndex(request, format=None):
         'snippets': reverse('snippetList', request=request, format=format)
     })
 
-# Using generic class-based views
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+""" Using generic class-based views
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -49,7 +67,8 @@ class UserList(generics.ListAPIView):
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSerializer 
+"""
 
 """ Using mixins
 class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
